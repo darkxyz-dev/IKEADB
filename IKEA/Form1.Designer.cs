e6f6d9d008bc42193be552ac61de3ac9f638b155
Opilane TTHK <opilane@tthk.ee>
2021-02-25 11:25:14 +0200

    first commit

---
+++ IKEA/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace IKEA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ikea = new System.Windows.Forms.Label();
+            this.listIkea = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listSeriesNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Ikea
+            // 
+            this.Ikea.AutoSize = true;
+            this.Ikea.Location = new System.Drawing.Point(99, 91);
+            this.Ikea.Name = "Ikea";
+            this.Ikea.Size = new System.Drawing.Size(28, 13);
+            this.Ikea.TabIndex = 0;
+            this.Ikea.Text = "Ikea";
+            // 
+            // listIkea
+            // 
+            this.listIkea.FormattingEnabled = true;
+            this.listIkea.Location = new System.Drawing.Point(102, 154);
+            this.listIkea.Name = "listIkea";
+            this.listIkea.Size = new System.Drawing.Size(120, 95);
+            this.listIkea.TabIndex = 1;
+            this.listIkea.SelectedIndexChanged += new System.EventHandler(this.listIkea_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Series Names";
+            // 
+            // listSeriesNames
+            // 
+            this.listSeriesNames.FormattingEnabled = true;
+            this.listSeriesNames.Location = new System.Drawing.Point(485, 154);
+            this.listSeriesNames.Name = "listSeriesNames";
+            this.listSeriesNames.Size = new System.Drawing.Size(120, 95);
+            this.listSeriesNames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listSeriesNames);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listIkea);
+            this.Controls.Add(this.Ikea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Ikea;
+        private System.Windows.Forms.ListBox listIkea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listSeriesNames;
+    }
+}
+
---
+++ IKEA/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IKEA
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["IKEA.Properties.Settings.IKEAConnectionString"].ConnectionString;
+        }
+
+        private void PopulateIKEATable()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Room", connection))
+            {
+                DataTable ikeaTable = new DataTable();
+                adapter.Fill(ikeaTable);
+
+                listIkea.DisplayMember = "RoomName";
+                listIkea.ValueMember = "Id";
+                listIkea.DataSource = ikeaTable;
+            }
+
+        }
+
+        private void PopulateIKEANames()
+        {
+            string query = "SELECT Series.Name From Room INNER JOIN Series ON Series.RoomId = Room.Id WHERE Room.Id = @RoomId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@RoomId", listIkea.SelectedValue);
+                DataTable seriesTableName = new DataTable();
+                adapter.Fill(seriesTableName);
+
+                listSeriesNames.DisplayMember = "SeriesName";
+                listSeriesNames.ValueMember = "Id";
+                listSeriesNames.DataSource = seriesTableName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateIKEATable();
+        }
+
+        private void listIkea_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateIKEANames();
+        }
+    }
+}
